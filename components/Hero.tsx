1bbdf9c4a9dd6e1250e09f702ad121b660ea4a43
sina saeedifar <sina.saeedifar@gmail.com>
2023-08-28 16:58:25 +0330

    1st commit

---
+++ components/Hero.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+
+function Hero() {
+  return (
+    <>
+      <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+          <h1 className="hero__title">
+            Find, Book or Rent a Car - Quickly and easily!
+          </h1>
+          <p className="hero__subtitle">
+            Stramline yout car Rental Exprience with our effortless booking
+            process.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Hero;
